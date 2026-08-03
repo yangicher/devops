be2fc19913728480c52794a9ee66ff5f56b8c0fe
Serge Iangicher <sergey.yangicher@rockyroad.io>
2026-08-03 12:33:55 +0300

    remove old

--- lesson-7/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "yangicher-devops-tf-state-2026"
-    key            = "lesson-7/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- lesson-7/modules/s3-backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  type        = string
-  description = "Ім'я S3 бакета для збереження стейту"
-}
-
-variable "table_name" {
-  type        = string
-  description = "Ім'я таблиці DynamoDB для блокування"
-}
